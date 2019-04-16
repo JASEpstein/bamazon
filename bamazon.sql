0ee1038605c204d6e37f3c6ce9049185bed9212c
Jamie Epstein <jasepstein@gmail.com>
2019-04-15 23:42:15 -0500

    Adds more items to inventory

--- bamazon.sql
+++ bamazon.sql
@@ -17,5 +17,10 @@ VALUES
     ("Arduino Uno 3", "Electronics", 34.95, 4),
     ("KitchenAid Standing Mixer", "Kitchen", 159.45, 3),
     ("Dishwasher Tablets", "Cleaning Products", 12.99, 6),
-    ("iPhone 6s", "Cell Phones", 649.34, 12);
+    ("iPhone 6s", "Cell Phones", 649.34, 12),
+    ("Game of Thrones DVD Set", "Entertainment", 32, 8),
+    ("NASA Flight Jacket", "Clothing", 150, 3),
+    ("MySQL Cookbook, by Paul DuBois", "Books", 45, 2),
+    ("Ryobi Hammer Drill", "Tools", 179, 4),
+    ("Inflatable Pool", "Outdoor Recreation", 45, 7);
 
